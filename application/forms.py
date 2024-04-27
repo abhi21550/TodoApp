113b6aa1435fa9fdd55c3a8de9952c293ec355bf
abhi21550 <abhinandmukundan15@gmail.com>
2024-04-27 16:02:59 +0530

    ZXZX

--- application/forms.py
+++ application/forms.py
@@ -6,4 +6,5 @@ class TodoForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = TextAreaField('description',validators=[DataRequired()])
     completed = SelectField('Completed', choices = [("False", "False"), ("True", "True")], validators = [DataRequired()])
+    todos = TextAreaField('todos',validators=[DataRequired()])
     submit = SubmitField("Add todo")
